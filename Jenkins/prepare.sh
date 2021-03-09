69d0f0da6c90760dbc99332d20620d6df4002ff5
wyb-bingo <704324033@qq.com>
2021-03-09 18:28:55 +0800

    修改shell

--- Jenkins/prepare.sh
+++ Jenkins/prepare.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-
 appName="stf:0.0.1-SNAPSHOT"
 word=`docker ps -a | grep $appName  |  awk '{print $1}'`
 echo "$word"
-
-if [ -z "$word" ];
+if [ -z "$word" ]
 then
    echo "当前不存在该容器，直接进行启动该操作-------------------------------------"
 else
